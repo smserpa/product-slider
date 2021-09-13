077dce8a31d9687105cd8713abb7394311a83c9e
Stefanie Serpa <smserpa@gmail.com>
2021-09-13 06:57:08 -0700

    added infinite scroll functionality by scrollY in onLoad function

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let clonesWidth;
 let sliderWidth;
 let clones = [];
 let disableScroll = false;
-let scrollPos;
+let scrollPosition;
 
 let items = [...document.querySelectorAll('.slider-item')];
 let images = [...document.querySelectorAll('.img-div')];
@@ -12,3 +12,54 @@ let images = [...document.querySelectorAll('.img-div')];
 images.forEach((image, index) => {
     image.style.backgroundImage = `url(./images/${index+1}.png)`
 })
+
+items.forEach(item => {
+    let clone = item.cloneNode(true);
+    clone.classList.add('clone');
+    slider.appendChild(clone);
+    clones.push(clone);
+})
+
+function getClonesWidth() {
+    let width = 0;
+    clones.forEach(clone => {
+        width += clone.offsetWidth;
+    })
+    return width;
+}
+
+function getScrollPosition() {
+    return window.scrollY;
+}
+
+function setScrollPosition(position) {
+    window.scrollTo({top: position})
+}
+
+function scrollUpdate() {
+    scrollPosition = getScrollPosition();
+    if(clonesWidth + scrollPosition >= sliderWidth) {
+        window.scrollTo({top: 1});
+    }else if(scrollPosition <= 0){
+        window.scrollTo({top: sliderWidth - clonesWidth - 1})
+    }
+
+    slider.style.transform = `translateX(${-window.scrollY}px)`
+
+    requestAnimationFrame(scrollUpdate)
+}
+
+function onLoad() {
+    calculateDimensions();
+    document.body.style.height = `${sliderWidth}px`;
+    window.scrollTo({top: 1});
+    scrollUpdate();
+
+}
+
+function calculateDimensions() {
+    sliderWidth = slider.getBoundingClientRect().width;
+    clonesWidth = getClonesWidth();
+}
+
+onLoad();
